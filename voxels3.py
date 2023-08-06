cb405186c4d1f8ff486cb105f4899f947c453541
AwokeKnowing <jamesdavidmorris@gmail.com>
2023-08-06 03:06:19 -0700

    finetune obstacle view

--- voxels3.py
+++ voxels3.py
@@ -18,6 +18,7 @@ import anglerdroid as a7
 import matplotlib.pyplot as plt
 
 def mb_box(sx,sy,sz,t,c):
+    c=[1,1,1]
     #create a box and return mesh and bounding box
     rb1 = o3d.geometry.TriangleMesh.create_box(width=sx, height=sy, depth=sz)
     rb1.paint_uniform_color(c)
@@ -38,15 +39,15 @@ def extract_boxbot(pcd,recalculate=False):
                             [(-rb1sx/2-.07),-rb1sy/2,fl],
                             [0.9, 0.1, 0.1])
 
-        rb2sx=.33
+        rb2sx=.36
         rb2sy=.43
         rb2sz=.55
         rb2bb, rb2 = mb_box(rb2sx,rb2sy,rb2sz,
-                            [(-rb2sx/2-.04),-rb2sy/2,fl],
+                            [(-rb2sx/2-.03),-rb2sy/2,fl],
                             [0.9, 0.5, 0.1])
         
         rb3sx=.22
-        rb3sy=.52
+        rb3sy=.46
         rb3sz=.22
         rb3bb, rb3 = mb_box(rb3sx,rb3sy,rb3sz,
                             [(-rb3sx/2+.00),-rb3sy/2,fl],
@@ -55,7 +56,7 @@ def extract_boxbot(pcd,recalculate=False):
         rb4sx=.16
         rb4sy=.22
         rb4sz=1.0
-        rb4bb, rb4 = mb_box(rb4sx,rb4sy,rb4sz,[(-rb4sx/2-.22),-rb4sy/2,fl],[0.4, 0.4, 0.6])
+        rb4bb, rb4 = mb_box(rb4sx,rb4sy,rb4sz,[(-rb4sx/2-.21),-rb4sy/2,fl],[0.4, 0.4, 0.6])
 
         boxbot_last_mesh = rb1+rb2+rb3+rb4
         boxbot_last_bboxes = (rb1bb,rb2bb,rb3bb,rb4bb)
@@ -93,7 +94,8 @@ def bbox_from_xxyyzz(x1,x2,y1,y2,z1,z2,color=(1, 0, 0)):
     return bbox
 
 
-def calc_unrotate_floor(pcd,ransac_dist=.003,ransac_n=3,ransac_iter=100):
+def calc_unrotate_floor(pcd,ransac_dist=.015,ransac_n=3,ransac_iter=100):
+    
     plane, points = pcd.segment_plane(distance_threshold=ransac_dist,
                                              ransac_n=ransac_n,
                                              num_iterations=ransac_iter)
@@ -146,14 +148,15 @@ def topview(vis):
 if __name__ == "__main__":
     with_color=False   #setting false really slows down on small voxel size
     with_forward=True
+    with_forward_alignment=False
     with_clusters=False
-    with_shadow_ground=True
+    with_shadow_ground=False
+    with_voxels=False 
 
-    show_voxels=False 
     show_pointcloud=True
-    show_axis=True
-    show_topdown_roi_box=True
-    show_boxbot=True
+    show_axis=False
+    show_topdown_roi_box=False
+    show_boxbot=False
     show_floor_basis_points=False
 
     crop_floor=True
@@ -180,26 +183,32 @@ if __name__ == "__main__":
     vis = Visualizer()
     
     #need to make a function to set view and capture frame and call it 1 fps
-    vis.create_window(width=240,height=424)
-    vis.get_render_option().point_size=28/voxel_detail
+    vis.create_window(width=224,height=224)
+    vis.get_render_option().point_size=4/voxel_detail
 
-    floor_depth_size=(240,424)
+    floor_depth_size=(224,224)
     floor_acc= np.zeros_like(floor_depth_size)
     
     pcd = PointCloud()
     floor_basis_points_pcd = PointCloud()
 
     #topdown roi volume points (for obstacles, ie excluding floor)
-    
-    tdx1 = -0.91874238
-    tdx2 = +0.88713181
-    tdy1 = -0.50779997
-    tdy2 = +0.54382918
-    tdz1 = -0.91435421
-    tdz2 = -0.22804920
-    floor_z = tdz1
+    min_obstacle_height=.005 #obstacles smaller than this will be ignored
+    tdx1 = -0.92
+    tdx2 = +0.92
+    tdy1 = -0.54
+    tdy2 = +0.54
+    tdz1 = -0.92
+    tdz2 = -0.0
+    floor_z = tdz1 + min_obstacle_height 
 
-    morph_kernel = np.ones((5, 5), np.uint8)
+    if with_forward:
+        tdx1 = -0.92
+        tdx2 = +3.56
+        tdy1 = -2.24
+        tdy2 = +2.24
+
+    morph_kernel = np.ones((3, 3), np.uint8)
     last_forward_transform = np.identity(4)
 
     #preallocate image
@@ -210,17 +219,19 @@ if __name__ == "__main__":
         while True:
 
             dt0=datetime.now()
-            tempsource,rgbd,intrinsic = topdowncam.frame()
+            tempsource,rgbd,intrinsic = topdowncam.frame(3,4)
+            topdown_pcd=tempsource
             topdown_pcd=tempsource.voxel_down_sample(voxel_size=voxel_size)
             
             if frame_count % 30 == 0:
+                #must be done on first frame to get initial rotation 
                 floor_inv_R,floor_Rcenter,floor_basis_points=calc_unrotate_floor(topdown_pcd)
             
             topdown_pcd = topdown_pcd.rotate(floor_inv_R, center=floor_Rcenter)
             
             
             if with_forward:
-                forward_pcd,rgbd2,intrinsic = forwardcam.frame()
+                forward_pcd,rgbd2,intrinsic = forwardcam.frame(3,4)
                 forward_pcd = forward_pcd.voxel_down_sample(voxel_size=voxel_size)
 
                 p=np.pi/2
@@ -237,27 +248,35 @@ if __name__ == "__main__":
                     #forward_pcd = forward_pcd.translate([.88,.40,.32])
                     forward_pcd = forward_pcd.translate([-.105,.035,-.525])
 
-                if False:
-                    topdown_pcd.estimate_normals()
-                    reg_p2p = o3d.pipelines.registration.registration_icp(
-                        forward_pcd, topdown_pcd, .02, np.identity(4),
-                        o3d.pipelines.registration.TransformationEstimationPointToPlane(),
-                        #o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=2000)
-                        )
-                    #forward_pcd.paint_uniform_color((0,1,0))
-                    #topdown_pcd.paint_uniform_color((1,0,0))
-                    #print("reg",reg_p2p)
-                    if True or reg_p2p.fitness > .04 and len(reg_p2p.correspondence_set)>150:
-                        forward_pcd.transform(reg_p2p.transformation)
-                        last_forward_transform = reg_p2p.transformation
-                    else:
-                        print("bad fit")
-                        forward_pcd.transform(last_forward_transform)
-         
+                if with_forward_alignment and frame_count % 30 ==15:
+                    topdown_pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=voxel_size *
+                                                     2,
+                                                     max_nn=30))
+                    if True:
+                        reg_p2p = o3d.pipelines.registration.registration_icp(
+                            forward_pcd, topdown_pcd, .01, np.identity(4),
+                            o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+                            o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=40)
+                            )
+                        #forward_pcd.paint_uniform_color((0,1,0))
+                        #topdown_pcd.paint_uniform_color((1,0,0))
+                        print("reg",reg_p2p)
+                        if  reg_p2p.fitness > .15 and len(reg_p2p.correspondence_set)>250:
+                            last_forward_transform = reg_p2p.transformation
+                        else:
+                            print("bad fit")
+                            #forward_pcd.transform(last_forward_transform)
+
+                forward_pcd.transform(last_forward_transform)
             
-            
-                            
-            
+            #cut the bot self cloud out before merging
+            topdown_pcd, boxbot_pcd, boxbot_mesh = extract_boxbot(topdown_pcd)
+
+            if with_forward:
+                topdown_pcd.points.extend(forward_pcd.points)
+                topdown_pcd.colors.extend(forward_pcd.colors)            
+                topdown_pcd.normals.extend(forward_pcd.normals)
+
             # because topdown 0,0 is the 'origin' of robot, we set 
             # the roi in worldspace rather than pcd space
             #above_ground_bbox = topdown_pcd.get_axis_aligned_bounding_box()
@@ -271,26 +290,6 @@ if __name__ == "__main__":
             if crop_floor:
                 topdown_pcd=topdown_pcd.crop(above_ground_bbox)
 
-            topdown_pcd, boxbot_pcd, boxbot_mesh = extract_boxbot(topdown_pcd)
-
-            if with_clusters:
-                labels = np.array(topdown_pcd.cluster_dbscan(eps=0.1, min_points=int(2.**voxel_detail)))
-
-                try:
-                    max_label = labels.max()    
-                except ValueError:
-                    max_label=0
-                #print(f"point cloud has {max_label + 1} clusters")
-                colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-                #colors[labels < 1] = 0
-                #temp.colors = o3d.utility.Vector3dVector(colors[:, :3])
-
-
-            if with_forward:
-                topdown_pcd.points.extend(forward_pcd.points)
-                topdown_pcd.colors.extend(forward_pcd.colors)            
-                topdown_pcd.normals.extend(forward_pcd.normals)
-
             if show_pointcloud:
                 #topdown_pcd.points[:,:,2]=0
                 pcd.points = topdown_pcd.points
@@ -302,19 +301,35 @@ if __name__ == "__main__":
                 floor_basis_points_pcd.colors = floor_basis_points.colors
                 floor_basis_points_pcd.normals = floor_basis_points.normals
             
-            if show_voxels:
+            if with_voxels:
                 vxgrid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=voxel_size)
+
+            if with_clusters:
+                labels = np.array(pcd.cluster_dbscan(eps=0.1, min_points=int(2.**voxel_detail)))
+
+                try:
+                    max_label = labels.max()    
+                except ValueError:
+                    max_label=0
+                #print(f"point cloud has {max_label + 1} clusters")
+                colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+                #colors[labels < 1] = 0
+                pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
 
             
             
         # draw 
             if frame_count ==0:
-                
+
+                vis.add_geometry(above_ground_bbox) #everything
+
                 if show_floor_basis_points:
                     vis.add_geometry(floor_basis_points_pcd)
 
                 if show_topdown_roi_box:
-                    vis.add_geometry(above_ground_bbox) #everything
+                    c=-.48
+                    above_ground_bbox_topdown = bbox_from_xxyyzz(tdx1-c,tdx2+c,tdy1-c,tdy2+c,floor_z,tdz2,(1, 0, 0))
+                    vis.add_geometry(above_ground_bbox_topdown) #everything
 
                 if show_boxbot:
                     vis.add_geometry(boxbot_mesh) #robot
@@ -325,7 +340,7 @@ if __name__ == "__main__":
                     mesh_frame1 = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=[1, 0, 0])
                     vis.add_geometry(mesh_frame1)
 
-                if show_voxels:
+                if with_voxels:
                     vxgrid_old = vxgrid
                     vis.add_geometry(vxgrid_old)
                 
@@ -343,7 +358,7 @@ if __name__ == "__main__":
             if show_floor_basis_points:
                 vis.update_geometry(floor_basis_points_pcd)
             
-            if show_voxels:
+            if with_voxels:
                 vis.remove_geometry(vxgrid_old,False)
                 vis.add_geometry(vxgrid,False)
                 vxgrid_old = vxgrid
@@ -356,10 +371,15 @@ if __name__ == "__main__":
 #2d render
             
             vis.update_renderer()
+            
+                
             floor_depth = np.array(vis.capture_depth_float_buffer()).astype(np.uint8)
-            floor_depth = cv2.resize(floor_depth, floor_depth_size, interpolation= cv2.INTER_LINEAR)
-            floor_depth = cv2.morphologyEx(floor_depth, cv2.MORPH_CLOSE, morph_kernel)
+            
+            #floor_depth = cv2.resize(floor_depth, floor_depth_size, interpolation= cv2.INTER_LINEAR)
+            
+            #floor_depth = cv2.cvtColor(floor_depth, cv2.COLOR_BGR2GRAY)
             ret, floor_depth = cv2.threshold(floor_depth, 0, 255, cv2.THRESH_BINARY)
+            #floor_depth = cv2.morphologyEx(floor_depth, cv2.MORPH_CLOSE, morph_kernel)
             #floor_depth = np.where(floor_depth>0, 255, 0).astype(np.uint8).copy()
 
             #floor_depth_save = floor_depth
@@ -372,17 +392,17 @@ if __name__ == "__main__":
 
                     shadow_ground_depth = np.array(vis.capture_depth_float_buffer(True)).astype(np.uint8)
                     vis.remove_geometry(ground_pcd,False)
-                    shadow_ground_depth = cv2.resize(shadow_ground_depth, floor_depth_size, interpolation=cv2.INTER_LINEAR)
+                    #shadow_ground_depth = cv2.resize(shadow_ground_depth, floor_depth_size, interpolation=cv2.INTER_LINEAR)
                     shadow_ground_depth = cv2.morphologyEx(shadow_ground_depth, cv2.MORPH_CLOSE, morph_kernel)
                     ret, shadow_ground_depth = cv2.threshold(shadow_ground_depth, 0, 255, cv2.THRESH_BINARY_INV)
                     floor_acc = shadow_ground_depth
                     #floor_acc = np.where(shadow_ground_depth>0, 0, 255).astype(np.uint8).copy()
                     
                     #cover the tail shadow
-                    cv2.rectangle(floor_acc, (100,276), (240-100,423), (0,0,0), -1)
+                    cv2.rectangle(floor_acc, (100,276), (240-100,423), (0,0,0), 1)
                 #print(floor_acc,floor_depth)
                 floor_depth=cv2.bitwise_or(floor_acc,floor_depth)
-                floor_depth = cv2.morphologyEx(floor_depth, cv2.MORPH_CLOSE, morph_kernel)
+                #floor_depth = cv2.morphologyEx(floor_depth, cv2.MORPH_CLOSE, morph_kernel)
             
             cv2.imshow('floor depth', floor_depth)
             #cv2.imwrite('botmask.png',floor_depth)
